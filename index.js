6ea85b4dd0dadb343dc92bb207a351d9b6ae8b97
andreqko <andreqkodev@gmail.com>
2024-09-11 12:11:02 +0300

    changed db connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const createDatabaseIfNotExists = require('./admin-db');
+const { Client } = require('pg');
 
 async function startUp() {
   await createDatabaseIfNotExists();
